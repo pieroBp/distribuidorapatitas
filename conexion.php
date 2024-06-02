1c04ff48630f09b549af56b8e9a6239a7a582aab
pieroBp <50091263+pieroBp@users.noreply.github.com>
2024-06-02 14:19:08 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -7,6 +7,7 @@
     if (mysqli_connect_errno()){
         echo "No se pudo conectar a la base de datos";
         exit();
+        //EJEMPLO
     }
     mysqli_select_db($conexion,$bd) or die("No se encuentra la base de datos");
     mysqli_set_charset($conexion,"utf8");
